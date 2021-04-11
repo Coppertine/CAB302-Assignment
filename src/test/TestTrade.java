19cfa12fb3315baa9643461a5b982d0997dd09b6
Coppertine <37494321+Coppertine@users.noreply.github.com>
2021-04-11 16:04:32 +1000

    Implemented empty tests for trades

--- src/test/TestTrade.java
+++ src/test/TestTrade.java
@@ -1,26 +1,145 @@
 package test;
 
+import main.com.cab302qut.java.Items.Asset;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class TestTrade {
+    private Asset testItem1;
+
+    @BeforeEach
+    void init() {
+        //Example Trades
+
+    }
 
    // Get trade
+    @Test
+    public void getTrade() {
+
+    }
+
     // Add open trade
+    @Test
+    public void addOpenTrade() {
+
+    }
+
     // Add closed trade
+    @Test
+    public void addClosedTrade() {
+
+    }
+
     // Set trade as closed
+    @Test
+    public void setTradeClosed() {
+
+    }
+
     // Set trade as open
-    // Get buying/selling user/organisation
+    @Test
+    public void setTradeOpen() {
+
+    }
+
+    // Get buying user
+    @Test
+    public void getBuyingUser() {
+
+    }
+
+    // Get selling user
+    @Test
+    public void getSellingUser() {
+
+    }
+
+    // Get buying organisation
+    @Test
+    public void getBuyingOrganisation() {
+
+    }
+
+    // Get selling organisation
+    @Test
+    public void getSellingOrganisation() {
+
+    }
+
     // Get asset from trade
+    @Test
+    public void getTradeAsset() {
+
+    }
+
     // Get price from trade
+    @Test
+    public void getTotalTradePrice() {
+
+    }
+
     // Get quantity from trade
+    @Test
+    public void getQuantity() {
+
+    }
+
     // Get price per asset from trade
+    @Test
+    public void getPricePerAsset() {
+
+    }
+
     // Add date to trade
+    @Test
+    public void addDate() {
+
+    }
+
     // Get date to trade
+    @Test
+    public void getDate() {}
+
     // Complete trade transaction of trade between organisations.
+    @Test
+    public void completeTransaction() {
+
+    }
+
     // Check balance of organisations if trade can be processed.
+    @Test
+    public void getBalanceOfOrganisations() {
+
+    }
     // Set asset to trade
+    @Test
+    public void setAsset() {
+
+    }
+
     // Set buying/selling user/organisation
+    @Test
+    public void setBuyingUser() {
+
+    }
+
+    @Test
+    public void setSellingUser() {
+
+    }
+
+    @Test
+    public void setBuyingOrganisation() {
+
+    }
+    @Test
+    public void setSellingOrganisation() {
+
+    }
     // Get trade status
-    //
+    @Test
+    public void getTradeStatus() {
+
+    }
 }
