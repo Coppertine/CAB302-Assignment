8e7e0cd7953c1bd8f17ea5c73751602723491a5e
Coppertine <37494321+Coppertine@users.noreply.github.com>
2021-04-11 15:08:40 +1000

    Add test outlines to Asset

--- src/test/TestAsset.java
+++ src/test/TestAsset.java
@@ -1,4 +1,13 @@
 package test;
 
 public class TestAsset {
+    // Create new Asset
+    // Delete asset (after asset is created)
+    // Get list of assets
+    // Change name of asset
+    // Get spesific asset by name
+    // Get asset name
+    // Get Id of newly created asset
+    // remove asset by id
+    // get asset from id
 }
