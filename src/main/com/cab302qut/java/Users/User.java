0505162fa67097a4c4a6e0e60bc59471b0d6bbab
smarkovics <smarkovics00@gmail.com>
2021-04-17 14:25:04 +1000

    added password hashing
    password hashing has been done as well as some more tests have been completed.

--- src/main/com/cab302qut/java/Users/User.java
+++ src/main/com/cab302qut/java/Users/User.java
@@ -2,6 +2,9 @@ package main.com.cab302qut.java.Users;
 
 import main.com.cab302qut.java.Organisation.Organisation;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 /**
  * The account of the program which can buy and/or sell items to other users in other organisations.
  */
@@ -40,7 +43,8 @@ public class User {
      * @param password The hashed password of specified user.
      */
     public void setPassword(String password) {
-        this.password = password;
+        String hashedPassword = HashPassword(password);
+        this.password = hashedPassword;
     }
 
     /**
@@ -86,9 +90,40 @@ public class User {
         return userType;
     }
 
+    /**
+     * hashes the password with the MD5 algorithm
+     *
+     * @param unhashed the users string input password
+     * @return the hashed password
+     */
     public String HashPassword(String unhashed) {
         //Needs to actually hash the password
-        String hashedPassword = unhashed;
-        return hashedPassword
+        String hashedPassword = null;
+        try {
+            //creates message digest object needed.
+            //built in class to help hash passwords
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            //Add password bytes to digest
+            md.update(unhashed.getBytes());
+            //Get the hash's bytes
+            byte[] bytes = md.digest();
+            System.out.println(md.digest());
+            //This bytes[] has bytes in decimal format;
+            //Convert it to hexadecimal format
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < bytes.length; i++) {
+                sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
+            }
+            //Get complete hashed password in hex format
+            hashedPassword = sb.toString();
+            //prints to console hashed password
+            System.out.println(hashedPassword);
+            return hashedPassword;
+
+            //catches if an algorithm doesn't exist
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
--- src/test/TestUser.java
+++ src/test/TestUser.java
@@ -1,6 +1,5 @@
 package test;
 
-
 import main.com.cab302qut.java.Organisation.Organisation;
 import main.com.cab302qut.java.Users.User;
 import main.com.cab302qut.java.Users.UserType;
@@ -31,18 +30,20 @@ public class TestUser {
     // Verify permission to modify organisation (name, users and Organisation asset)
     //
 
-    private User[] users;
+    private User[] users = new User[3];
     private String username = "johnSmith";
     private String name = "john";
     private String password = "Test";
     private User testUser;
     private User testUser1;
     private Organisation testOrganisation1;
+    private Organisation testOrganisation2;
     private UserType userType1;
 
     @BeforeEach
     void init() {
         testOrganisation1 = new Organisation("organisation1");
+        testOrganisation2 = new Organisation("organisation2");
         userType1 = UserType.Administrator;
         testUser = new User(name, testOrganisation1, username, password, userType1);
         testUser1 = new User("John1", testOrganisation1, "JohnSmith1", "", userType1);
@@ -52,9 +53,9 @@ public class TestUser {
     //maybe change
     public void AddUser() {
         users[0] = testUser;
-        assertEquals(users[0].getName(), "John");
+        assertEquals(users[0].getName(), "john");
         assertEquals(users[0].getOrganisation(), testOrganisation1);
-        assertEquals(users[0].getUsername(), "JohnSmith");
+        assertEquals(users[0].getUsername(), "johnSmith");
         assertEquals(users[0].getPassword(), "Test");
         assertEquals(users[0].getUserType(), userType1);
     }
@@ -70,55 +71,61 @@ public class TestUser {
     }
 
     @Test
-    public void GetUsername(){
-        assertEquals(testUser.getUsername(), "JohnSmith");
+    public void GetUsername() {
+        assertEquals(testUser.getUsername(), "johnSmith");
     }
 
     @Test
-    public void ModifyUsername(){
+    public void ModifyUsername() {
         testUser.setUsername("newUsername");
         assertEquals(testUser.getUsername(), "newUsername");
     }
 
     @Test
-    public void SetPassword(){
+    public void SetPassword() {
 
-        assertEquals(testUser.getPassword(), "");
+        //assertEquals(testUser.getPassword(), "");
         testUser1.setPassword("newpassword");
-        assertEquals(testUser.getPassword(), "newpassword");
+        assertEquals(testUser1.getPassword(), "5e9d11a14ad1c8dd77e98ef9b53fd1ba");
     }
 
+    //I think this test is unneeded as the password is hashed when set password is called.
     @Test
-    public void HashPassword(){
-
-    }
-
-    @Test
-    public void GetHashedPassword(){
+    public void HashPassword() {
 
     }
 
+    //I think this test is also unneeded.
     @Test
-    public void GetOrganisation(){
-
-    }
-    @Test
-    public void SetOrganisation(){
+    public void GetHashedPassword() {
 
     }
 
     @Test
-    public void RemoveOrganisation(){
+    public void GetOrganisation() {
+testUser1.getOrganisation();
+        assertEquals(testUser1.getOrganisation(), testOrganisation1);
+    }
+
+    @Test
+    public void SetOrganisation() {
+        //testUser.setOrganisation(testOrganisation1);
+        testUser1.setOrganisation(testOrganisation2);
+        assertEquals(testUser1.getOrganisation(), testOrganisation2);
+    }
+
+    @Test
+    public void RemoveOrganisation() {
 //will throw exception as user must have organisation
     }
 
     @Test
-    public void GetUserType(){
+    public void GetUserType() {
         assertEquals(testUser.getUserType(), UserType.Administrator);
     }
 
     @Test
-    public void SetUserType(){
+    public void SetUserType() {
 
     }
 
