c8bd5276e25f546dfd3134dcd9761ca416545fb5
Coppertine <37494321+Coppertine@users.noreply.github.com>
2021-05-02 14:58:12 +1000

    Attempt to add Server side FXML and Config

--- src/main/com/cab302qut/java/util/DatabaseConnection.java
+++ src/main/com/cab302qut/java/util/DatabaseConnection.java
@@ -1,9 +1,30 @@
 package com.cab302qut.java.util;
 
+import com.cab302qut.java.CAB302Assignment;
+
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class DatabaseConnection {
     private static Connection connection;
 
+
+    public static void establishConnection() {
+        try {
+            ServerConfiguration serverConfig = CAB302Assignment.getConfig();
+            Class.forName("org.mariadb.jdbc.Driver");
+            System.out.println("MariaDB JDBC Driver Registered!");
+            connection = DriverManager.getConnection(
+                    "jdbc:mariadb://"+ serverConfig.getAddress()
+                            + ":" + serverConfig.getPort() +
+                            "/" + serverConfig.getSchema(),
+                    serverConfig.getUsername(),
+                    serverConfig.getPassword());
+
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+    }
 
 }
