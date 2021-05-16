993120f6e2805c092bb50f5a78bfd0bfa633c48d
Giane <n10532935@qut.edu.au>
2021-05-16 15:56:46 +1000

    Initial code for organisation's trade history page functionality

---
+++ src/main/com/cab302qut/java/Controller/OrganisationTradeHistory.java
@@ -0,0 +1,20 @@
+package com.cab302qut.java.Controller;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import org.w3c.dom.Text;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class OrganisationTradeHistory implements Initializable {
+
+
+    @FXML
+    private Text organisationLabel;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
