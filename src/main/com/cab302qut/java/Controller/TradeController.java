0221587e8920081d7e47642086983be7aef5a2e3
smarkovics <smarkovics00@gmail.com>
2021-05-16 15:01:48 +1000

    create trade screen design complete

--- src/main/com/cab302qut/java/Controller/TradeController.java
+++ src/main/com/cab302qut/java/Controller/TradeController.java
@@ -3,10 +3,7 @@ package com.cab302qut.java.Controller;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.text.Text;
 import javafx.scene.text.TextFlow;
 
@@ -14,5 +11,26 @@ import com.cab302qut.java.Users.User;
 
 import java.io.IOException;
 public class TradeController {
+
+    @FXML
+    private ChoiceBox assetChoice;
+    @FXML
+    private ToggleButton buyButton;
+    @FXML
+    private ToggleButton sellButton;
+    @FXML
+    private TextField assetQuantity;
+    @FXML
+    private TextField assetPrice;
+    @FXML
+    private Button backButton;
+    @FXML
+    private Button sendOrder;
+
+
+//need to add different assets to choice box
+//then when clicked they are displayed to the user
+
+
 
 }
